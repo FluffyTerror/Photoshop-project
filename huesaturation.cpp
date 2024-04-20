254c479246c51fc872eebe27190b91aca83977af
C++ torturer <8353207@gmail.com>
2024-04-20 18:04:54 +0300

    hue
    по другому никак не сделать так что только так

--- huesaturation.cpp
+++ huesaturation.cpp
@@ -8,6 +8,26 @@ huesaturation::huesaturation(QWidget *parent) :
     ui->setupUi(this);
     ui->HueSlider->setRange(0, 360);
     ui->SaturationSlider->setRange(-100,100);
+    QString gradientColors;
+    for (int i = 0; i < 360; ++i) {
+        QString color = QColor::fromHsv(i, 255, 255).name();
+        gradientColors += QString("stop:%1 %2,").arg(QString::number(i / 360.0)).arg(color);
+    }
+
+    QString styleSheet = QString("QSlider::groove:horizontal {"
+                                 "    border: 1px solid #999999;"
+                                 "    height: 10px;"
+                                 "    margin: 2px 0;"
+                                 "    background: qlineargradient(x1:0, y1:0, x2:1, y2:0, %1);"
+                                 "}"
+                                 "QSlider::handle:horizontal {"
+                                 "    background-color: #ffffff;"
+                                 "    border: 1px solid #5c5c5c;"
+                                 "    width: 18px;"
+                                 "    margin: -2px 0;"
+                                 "    border-radius: 3px;"
+                                 "}").arg(gradientColors);
+    ui->HueSlider->setStyleSheet(styleSheet);
 }
 
 huesaturation::~huesaturation()
