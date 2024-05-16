5e24304d4caccfb8b70f34055980805944a7af91
Alto <125269058+AltoKlef@users.noreply.github.com>
2024-05-16 19:23:23 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -570,7 +570,6 @@ void MainWindow::changeColorPallete(QColor NewColor)
     ui->graphicsView->setSceneRect(PalettePixmapItem->boundingRect());
     ui->graphicsView->fitInView(PalettePixmapItem, Qt::KeepAspectRatio);
 
-    //CopyColorImage=CopyColorImageT.copy();
     // Генерируем сигнал для разрешения добавления нового цвета
     emit color_pallete_allow_adding_a_new_color(NewColor);
 
